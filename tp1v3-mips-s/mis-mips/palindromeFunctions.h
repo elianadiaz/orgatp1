f675596189f828818b99b7f75c43c822b8efc763
ediaz <diazeliana09@gmail.com>
2017-10-21 20:49:07 -0700

    Se hacen ajustes al codigo mips para que compile y funcione correctamente.

---
+++ tp1v3-mips-s/mis-mips/palindromeFunctions.h
@@ -0,0 +1,21 @@
+/*
+ * palindromeFunctions.h
+ *
+ *  Created on: Oct 18, 2017
+ *      Author: ediaz
+ */
+
+#ifndef PALINDROMEFUNCTIONS_H_
+#define PALINDROMEFUNCTIONS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <getopt.h>
+#include <unistd.h>
+
+#include "constants.h"
+
+extern int palindrome(int ifd, size_t ibytes, int ofd, size_t obytes);
+
+#endif /* PALINDROMEFUNCTIONS_H_ */
